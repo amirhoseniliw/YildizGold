d5943ca736d04b83f7b84e7ffe02087d058efa8a
tools.iliw@gmail.com <tools.iliw@gmail.com>
2025-07-31 19:24:47 +0330

    update

--- resources/views/components/sider.blade.php
+++ resources/views/components/sider.blade.php
@@ -5,32 +5,44 @@
                 </div>
             </div>
             <div class="sidebar-menu">
-                <a style="all: unset;" href=""><div class="menu-item">
-                    <i class="fas fa-home"></i>
-                    <span class="menu-text">داشبورد</span>
-                </div></a>
-                <div class="menu-item active">
-                    <i class="fas fa-shopping-cart"></i>
-                    <span class="menu-text">سفارشات</span>
-                </div>
-                <div class="menu-item">
-                    <i class="fas fa-box"></i>
-                    <span class="menu-text">محصولات</span>
-                </div>
-                <div class="menu-item">
-                    <i class="fas fa-users"></i>
-                    <span class="menu-text">مشتریان</span>
-                </div>
-                <div class="menu-item">
+                <a style="all: unset;" href="{{ route('admin_panel_index') }}">
+    <div class="menu-item {{ Route::currentRouteName() == 'admin_panel_index' ? 'active' : '' }}">
+        <i class="fas fa-home"></i>
+        <span class="menu-text">داشبورد</span>
+    </div>
+</a>
+
+<a style="all: unset;" href="{{ route('admin_panel_orders') }}">
+    <div class="menu-item {{ Route::currentRouteName() == 'admin_panel_orders' ? 'active' : '' }}">
+        <i class="fas fa-shopping-cart"></i>
+        <span class="menu-text">سفارشات</span>
+    </div>
+</a>
+
+<a style="all: unset;" href="{{ route('admin_panel_products') }}">
+    <div class="menu-item {{ Route::currentRouteName() == 'admin_panel_products' ? 'active' : '' }}">
+        <i class="fas fa-box"></i>
+        <span class="menu-text">محصولات</span>
+    </div>
+</a>
+
+<a style="all: unset;" href="{{ route('admin_panel_users') }}">
+    <div class="menu-item {{ Route::currentRouteName() == 'admin_panel_users' ? 'active' : '' }}">
+        <i class="fas fa-users"></i>
+        <span class="menu-text">مشتریان</span>
+    </div>
+</a>
+
+                <a style="all: unset;" href=""> <div class="menu-item">
                     <i class="fas fa-chart-bar"></i>
                     <span class="menu-text">گزارشات</span>
-                </div>
-                <div class="menu-item">
+                </div></a>
+                <a style="all: unset;" href=""> <div class="menu-item">
                     <i class="fas fa-cog"></i>
                     <span class="menu-text">تنظیمات</span>
-                </div>
-                <div class="menu-item">
+                </div></a>
+               <a style="all: unset;" href="">  <div class="menu-item">
                     <i class="fas fa-sign-out-alt"></i>
                     <span class="menu-text">خروج</span>
-                </div>
+                </div></a>
             </div>
